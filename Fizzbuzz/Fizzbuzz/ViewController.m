56f838fc298494f8224578751506e171b65997a8
douht <douht@ncs.com.sg>
2016-03-14 22:32:46 +0800

    puzzle done.

--- Fizzbuzz/Fizzbuzz/ViewController.m
+++ Fizzbuzz/Fizzbuzz/ViewController.m
@@ -19,6 +19,40 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(IBAction)startBtnClicked:(id)sender{
+    
+    for (int i = 1; i <= 100; i++) {
+        if (i%15 == 0) {
+            NSLog(@"Fizz Buzz");
+        }else if (i%5 == 0){
+            
+            NSLog(@"Buzz");
+        }else if (i%3 == 0){
+            
+            NSLog(@"Fizz");
+        }else{
+            
+            NSLog(@"%d",i);
+        }
+        
+    }
+    
+    UIAlertController * alert=   [UIAlertController
+                                  alertControllerWithTitle:@"Puzzle Done"
+                                  message:@"Print in the console."
+                                  preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
