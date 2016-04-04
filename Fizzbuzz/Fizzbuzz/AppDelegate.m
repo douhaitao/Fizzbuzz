3b96765f54bcf901a2caaefb0cba5a034c814d52
Haitao DOU <haitao.dou83@gmail.com>
2016-04-04 16:03:18 +0800

    add a test note

--- Fizzbuzz/Fizzbuzz/AppDelegate.m
+++ Fizzbuzz/Fizzbuzz/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //HT kept a note here.
     return YES;
 }
 
